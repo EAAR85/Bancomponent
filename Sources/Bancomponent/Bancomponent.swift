98b1c255cfca4d767fd2aa6419d02e83ea59a0bf
EAAR85 <thrive.itec@gmail.com>
2023-10-22 10:50:04 -0500

    first commit

---
+++ Sources/Bancomponent/Bancomponent.swift
@@ -0,0 +1,10 @@
+public struct Bancomponent {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+    
+    public func hi() -> String {
+        return text
+    }
+}
